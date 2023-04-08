3e532cb04dceca3b2e21acd9c747c4e2f0993670
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-04-08 23:05:09 +0500

    trying role base component abstraction

---
+++ src/components/buyer/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Buyer: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Buyer;
---
+++ src/components/seller/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Seller: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Seller;
