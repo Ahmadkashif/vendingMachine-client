fd8314fc6ca65d6ba89448c26d243a3664ef33b4
Ahmad Kashif <ahmadkashif983@gmail.com>
2023-04-08 23:43:22 +0500

    adding types based on models

--- src/components/buyer/index.tsx
+++ src/components/buyer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Buyer: React.FC = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
--- src/components/seller/index.tsx
+++ src/components/seller/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Seller: React.FC = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
